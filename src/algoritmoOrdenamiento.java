ad1795cf54c83eef07bca730180a7fcabb6c4e7e
EMMANUEL <Hi @EmmanuelRB!>
2017-11-30 23:15:57 -0600

    diferentes algoritmos de ordenamiento

--- src/algoritmoOrdenamiento.java
+++ src/algoritmoOrdenamiento.java
@@ -32,7 +32,85 @@ public class algoritmoOrdenamiento {
             }
         }
     }
+    //metodo radix
+    public void radix(int[]arreglo){
+        int x, i,j;
+        for(x=Integer.SIZE-1; x>=0; x--){
+            int auxiliar[]= new  int[arreglo.length];
+            j=0;
+            for (i=0; i<arreglo.length; i++){
+                boolean mover=arreglo[i]<<x>=0;
+                if (x==0? !mover:mover){
+                    auxiliar[j]=arreglo[i]; j++;
+
+                }else {
+                    arreglo[i-j]=arreglo[i];
+                }
+            }
+            for (i=j; i<auxiliar.length;i++){
+                auxiliar[i]= arreglo[i=j];
+
+            }
+            arreglo=auxiliar;
+
+        }
+        System.out.print("el arreglo ordenado con radix");
+        mostrarArreglo(arreglo);
+    }
+
+    // metodo quick
     //mostrar los datos del vector
+
+    public void quick(int[]arreglo, int primero, int ultimo){
+        int i,j,pivote,auxiliar;
+        i=primero;
+        j=ultimo;
+        pivote=arreglo[(primero+ultimo)/2];
+        System.out.println("el pivote es: "+pivote);
+        do{
+            while (arreglo[i]<pivote){
+                i++;
+            }
+            while (arreglo[j]>pivote){
+                j--;
+            }
+            if (i<=j){
+                auxiliar=arreglo[i];
+                arreglo[i]=arreglo[j];
+                arreglo[j]=auxiliar;
+                i++;
+                j--;
+
+            }
+
+
+        }while (i<=j);
+        if(primero<j){
+            quick(arreglo,primero,j);
+
+        }
+        if(i<ultimo){
+            quick(arreglo,i,ultimo);
+        }
+        mostrarArreglo(arreglo);
+
+    }
+    //metodo para inserccion
+    public void inserccion(int [] arreglo, int n){
+        int i,j, auxiliar;
+        for(i=1; i<n; i++){
+            auxiliar=arreglo[i];
+                    j=i-1;
+            while (j>0 && arreglo[j]>auxiliar){
+                arreglo[j+1]=arreglo[j];
+                j=j-1;
+            }
+            arreglo[j+1]=auxiliar;
+        }
+        System.out.println("Arreglo ordenado por inserccion ");
+        mostrarArreglo(arreglo);
+    }
+
 
     public void mostrarArreglo(int[]arreglo){
        int k=0;
--- src/programa.java
+++ src/programa.java
@@ -1,22 +1,43 @@
+import javax.swing.*;
+
 public class programa {
     public static void main(String[]args){
         algoritmoOrdenamiento ordenar= new algoritmoOrdenamiento();
         int vector1[]={5,6,3,44,22,1};
         int vector2[]={55,4,43,44,2,10};
-        System.out.println("arreglo original ");
+        int vector3[]={5,2,1,8,3,9,7};
+        /*System.out.println("arreglo original ");
         ordenar.mostrarArreglo(vector1);
 
         System.out.println("arreglo ordenado con burbuja 1");
         ordenar.burbuja1(vector1);
-        ordenar.mostrarArreglo(vector1);
+        ordenar.mostrarArreglo(vector1);*/
 
 
+
+        //System.out.println("arreglo original ");
+        /*ordenar.mostrarArreglo(vector2);
         System.out.println("arreglo original ");
-        ordenar.mostrarArreglo(vector2);
-
-        System.out.println("arreglo ordenado con burbuja 2");
+        System.out.println("arreglo ordenado con radix es: ");
         ordenar.burbuja1(vector2);
         ordenar.mostrarArreglo(vector2);
+        ordenar.mostrarArreglo(vector2);
+        ordenar.mostrarArreglo(vector3);
+        System.out.println("arreglo ordenado con quick ");
+        ordenar.quick(vector3,0,vector3.length-1);*/
+
+        int tam;
+        tam=Integer.parseInt(JOptionPane.showInputDialog(null,"ingrese el tamaño del vector"));
+        int vectorcito[]=new int[tam];
+        for (int i=0; i<tam; i++){
+            vectorcito[i]=Integer.parseInt(JOptionPane.showInputDialog(null,
+                    "ingresa el elemento del indice"+i));
+
+            ordenar.inserccion(vectorcito,i+1);
+        }
+
+
+
 
     }
 }
